9febfe6eb7e069077401060943f087983d397132
Volition21 <Volition21@Hackion.com>
2016-08-19 02:26:28 +0800

    Added a secondary check to ensure PPP tunnel creation succeeded.

--- checkVPN.sh
+++ checkVPN.sh
@@ -5,11 +5,16 @@
 
 # Throw this in a crontab with */1 * * * * for maximium tunnel capacity.
 
-vpnCheck=$(ifconfig | grep ppp0 | wc -l);
+if [ "$EUID" -ne 0 ]; then
+	echo "Please run as root"
+	exit
+fi
+
+ppp0Check1=$(ifconfig | grep -c ppp0);
  
-if [ $vpnCheck = 0 ]; then
+if [ "$ppp0Check1" = 0 ]; then
 	echo "ppp0 interface non-existant, assuming tunnel is down."
-	echo "Attempting to reconnect PPP tunnel"
+	echo "Attempting to reconnect PPP tunnel over SSH"
 	
 	# updetach = PPPD will detatch from current terminal device once connection established.
 	# noauth = Peer authnetication not required, this option is privilaged.
@@ -23,8 +28,19 @@ if [ $vpnCheck = 0 ]; then
 	
 	pppd updetach noauth silent nodeflate pty "/usr/bin/ssh jogw@cassa.org.au -i /root/.ssh/jogw sudo /usr/sbin/pppd nodetach notty noauth" 172.16.18.1:172.16.18.2
 	sleep 10
-	echo "Replacing default route with PPP endpoint"
-	ip route replace default via 172.16.18.1
+	
+	ppp0Check2=$(ifconfig | grep -c ppp0);
+	
+	if [ "$ppp0Check2" = 1 ]; then
+		echo "PPP Tunnel interface is present on ppp0"
+		echo "Replacing default route with ppp0 endpoint"
+		ip route replace default via 172.16.18.1
+		echo "Tunnel appears to be active."
+	else
+		echo "There was an error creating the PPP interface, ppp0 does not exist."
+		exit
+	fi
 else
-	echo "PPP Tunnel Should be up."
+	echo "PPP Tunnel interface is present on ppp0."
 fi
+
