ffc2fb4502387e49e52e42884d8fb2007a6cc9e1
Volition21 <Volition21@Hackion.com>
2016-08-18 14:02:46 +0800

    Changed to check for interface rather than ICMP response

--- checkVPN.sh
+++ checkVPN.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-pingCheck=$(ping -c 1 8.8.8.8 | grep ", 0% packet loss" | wc -l);
+vpnCheck=$(ifconfig | grep ppp0 | wc -l);
  
-if [ $pingCheck = 0 ]; then
+if [ $vpnCheck = 0 ]; then
 	echo "Restarting VPN as it is down :("
-	pppd  updetach noauth silent noauth silent nodeflate pty "/usr/bin/ssh jogw@cassa.org.au -i /root/.ssh/jogw sudo /usr/sbin/pppd nodetach notty noauth"  ipparam vpn 
-172.16.18.1:172.16.18.2
-	sleep 5
+	pppd  updetach noauth silent noauth silent nodeflate pty "/usr/bin/ssh jogw@cassa.org.au -i /root/.ssh/jogw sudo /usr/sbin/pppd nodetach notty noauth"  ipparam vpn 172.16.18.1:172.16.18.2
+	sleep 10
 	ip route replace default via 172.16.18.1
 else
 	echo "VPN is up :)"
 fi
-echo "VPN SHOULD BE UP!"
 
