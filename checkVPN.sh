b6c7b718f3dab74573d225ce856482c77ab5c9a7
Volition21 <Volition21@Hackion.com>
2016-08-19 12:01:38 +0800

    Working with WDR4300

--- checkVPN.sh
+++ checkVPN.sh
@@ -5,9 +5,9 @@
 
 # Throw this in a crontab with */1 * * * * for maximium tunnel capacity.
 
-ppp0Check1=$(ifconfig | grep -c ppp0);
- 
-if [ "$ppp0Check1" = 0 ]; then
+vpnCheck=$(ifconfig | grep ppp0 | wc -l);
+
+if [ $vpnCheck = 0 ]; then
 	echo "ppp0 interface non-existant, assuming tunnel is down."
 	echo "Attempting to reconnect PPP tunnel over SSH"
 	
@@ -21,15 +21,13 @@ if [ "$ppp0Check1" = 0 ]; then
 	# notty = Create character shunt to transfer characters over STDIN & STDOUT without being connected to a terminal device.
 	# noauth = Peer authnetication not required, this option is privilaged.
 	
-	pppd updetach noauth silent nodeflate pty "/usr/bin/ssh jogw@cassa.org.au -i /root/.ssh/jogw sudo /usr/sbin/pppd nodetach notty noauth" 172.16.18.1:172.16.18.2
+	pppd updetach noauth silent noauth silent nodeflate pty "/usr/bin/ssh jogw@cassa.org.au -i /root/.ssh/jogw sudo /usr/sbin/pppd nodetach notty noauth"  ipparam vpn 172.16.18.1:172.16.18.2
 	sleep 10
-	
-	ppp0Check2=$(ifconfig | grep -c ppp0);
-	
-	if [ "$ppp0Check2" = 1 ]; then
-		echo "PPP Tunnel interface is present on ppp0"
-		echo "Replacing default route with ppp0 endpoint"
-		ip route replace default via 172.16.18.1
+	echo "Replacing default route with ppp0 endpoint"
+	ip route replace default via 172.16.18.1
+	sleep 1
+	vpnCheck=$(ifconfig | grep ppp0 | wc -l);
+	if [ $vpnCheck = 1 ]; then
 		echo "Tunnel appears to be active."
 	else
 		echo "There was an error creating the PPP interface, ppp0 does not exist."
